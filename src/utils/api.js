1ada24219ab24ce2f4a14745ccea144c3e4252c5
Prakhar Buliya <prakharbuliya02@gmail.com>
2023-06-08 14:12:58 +0530

    Added Searched Results Section

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,15 @@
 import axios from "axios";
 
-export const fetchDataFromApi = () => {};
+const BASE_URL = "https://www.googleapis.com/customsearch/v1";
+
+const params = {
+  key: "AIzaSyBKDBcVZ54LUSfV-1FTOPHdLHEW8IDEuIw",
+  cx: "26f478d921b824360",
+};
+
+export const fetchDataFromApi = async (payload) => {
+  const { data } = await axios.get(BASE_URL, {
+    params: { ...params, ...payload },
+  });
+  return data;
+};
